af6c1887cca95632154a319aad3e344867595d8e
Your Name <you@example.com>
2020-02-03 14:36:45 +0000

    commit

---
+++ src/main/java/com/qa/tutorial/Interaction.java
@@ -0,0 +1,17 @@
+package com.qa.tutorial;
+
+
+public class Interaction {
+
+
+
+	public String greeting() {
+		return "Hi friend";
+		
+	}
+ 
+	public String insult() {
+		return "Is trash";
+		
+	}
+}
